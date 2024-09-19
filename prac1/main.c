dd9e1337555abbbd495ef38c52ba96cce8c97c3a
shylit <trisvetovrichard@gmail.com>
2024-09-19 17:13:43 +0300

    Add files via upload

---
+++ prac1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void exampleFunction(int arg1, int arg2);
+
+int main() {
+    int x = 10;
+    int y = 20;
+    exampleFunction(x, y);
+    return 0;
+}
+
+void exampleFunction(int arg1, int arg2) {
+    // Function implementation
+}
