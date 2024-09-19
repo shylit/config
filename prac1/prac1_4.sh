d490d25e8eef0c0cffcdf06cb4dd62049eb69bc9
shylit <trisvetovrichard@gmail.com>
2024-09-19 17:15:18 +0300

    Update prac1_4.sh

--- prac1/prac1_4.sh
+++ prac1/prac1_4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Проверка наличия одного аргумента
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <file>"
     exit 1
@@ -8,13 +8,13 @@ fi
 
 file=$1
 
-# Проверка существования файла
+
 if [ ! -f "$file" ]; then
     echo "File not found!"
     exit 1
 fi
 
-# Извлечение идентификаторов из файла
+
 grep -oE '\b[a-zA-Z_][a-zA-Z0-9_]*\b' "$file" | \
-    grep -vE '^[0-9]' |                            # Исключаем слова, начинающиеся с цифры
-    sort | uniq                                    # Сортировка и удаление повторов
+    grep -vE '^[0-9]' |                            
+    sort | uniq                                    
